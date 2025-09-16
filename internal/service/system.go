1327cdd2cc41644641fc682c25afc64ab4543905
willem <18710090979@163.com>
2025-09-16 10:57:10 +0800

    feat: 1. 用户增加CODE业务处理

--- internal/service/system.go
+++ internal/service/system.go
@@ -47,13 +47,13 @@ type (
 		// JobLogList 获取任务日志列表
 		JobLogList(ctx context.Context, input *model.GetJobLogListInput) (total int, out []*model.SysJobLogOut, err error)
 		// AddJobLog 添加任务日志
-		AddJobLog(ctx context.Context,input *model.SysJobLogAddInput) (err error)
+		AddJobLog(ctx context.Context, input *model.SysJobLogAddInput) (err error)
 		// DelJobLogByIds 根据ID删除任务日志
 		DelJobLogByIds(ctx context.Context, ids []int) (err error)
 		// Export 导出任务日志列表
 		Export(ctx context.Context, input *model.GetJobLogListInput) (err error)
 		// ClearJobLogByDays 清理指定天数的定时任务日志
-		ClearJobLogByDays(ctx context.Context,days int) (err error)
+		ClearJobLogByDays(ctx context.Context, days int) (err error)
 	}
 	ISysLoginLog interface {
 		Invoke(ctx context.Context, data *model.LoginLogParams)
